5a63c25808d25607c0ac4955cff17421ae92b0eb
Christopher Orth <christopherorth430@yahoo.com>
2023-09-15 22:34:37 -0400

    completed but not passing their personal tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
 
+  const [isDark, setisDark] = useState(false)
+
+  function darkMode() {
+    setisDark((isDark) => !isDark)
+  }
+
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const appClass = !isDark ? "App dark" : "App light"
+  const mode = !isDark ? "click for Light Mode" : "click for Dark Mode"
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={darkMode}>{mode}</button>    {/* where it says mode they want "Dark Mode" */}
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [isinCart, setinCart] = useState(false)
+  
+  function addToCart() {
+    setinCart(!isinCart)
+  }
+  
   return (
-    <li className="">
+    <li className={isinCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={addToCart} className={!isinCart ? "add" : "remove"}>{isinCart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
